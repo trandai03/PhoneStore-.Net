6a137de57ae67c6607a839f93702a66a49f03c1e
Sonnnnkl03 <94890686+Sonnnnkl03@users.noreply.github.com>
2024-03-07 23:13:28 +0700

    Revert "Merge branch 'main' of https://github.com/trandai03/PhoneStore.Net"
    This reverts commit b8bc9ffc13ba4b26bccfa17ee2e66ab2efd5ccfd, reversing
    changes made to e0c561429f468a495bfbdc96848383cfab6e122c.

--- PhoneStore.Net/PhoneStore.Net/MainViewModel.cs
+++ PhoneStore.Net/PhoneStore.Net/MainViewModel.cs
@@ -95,7 +95,7 @@ namespace PhoneStore.Net
                     }
                 case 2:
                     {                
-                        p.Main.NavigationService.Navigate(new QLSP());
+                        p.Main.NavigationService.Navigate(new Detail_phone());
                         break;
                     }
                 case 3:
