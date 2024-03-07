e5e5db69bf6e9f89c30dca547614d232c214a7e2
manh tam <manhtam96@gmail.com>
2024-03-07 17:26:26 +0700

    update

--- PhoneStore.Net/PhoneStore.Net/MainViewModel.cs
+++ PhoneStore.Net/PhoneStore.Net/MainViewModel.cs
@@ -2,18 +2,24 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using PhoneStore.Net.View;
+using System.Windows;
 
 namespace PhoneStore.Net
 {
-    public class RelayCommand : ICommand
+    public class RelayCommand<T> : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Func<object, bool> _canExecute;
+        private readonly Predicate<T> _canExecute;
+        private readonly Action<T> _execute;
 
-        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        public RelayCommand(Predicate<T> canExecute, Action<T> execute)
         {
-            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            if (execute == null)
+                throw new ArgumentNullException("execute");
             _canExecute = canExecute;
+            _execute = execute;
         }
 
         public event EventHandler CanExecuteChanged
@@ -22,58 +28,119 @@ namespace PhoneStore.Net
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+        public bool CanExecute(object parameter)
+        {
+            try
+            {
+                return _canExecute == null ? true : _canExecute((T)parameter);
+            }
+            catch
+            {
+                return true;
+            }
+        }
 
-        public void Execute(object parameter) => _execute(parameter);
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
     }
 
     public class MainViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if (!(object.Equals(field, newValue)))
-            {
-                field = (newValue);
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-                return true;
-            }
-
-            return false;
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
         }
-        public ICommand SwitchToHomePageCommand { get; private set; }
-        public ICommand SwitchToOrdersPageCommand { get; private set; }
-        public ICommand SwitchToProductsPageCommand { get; private set; }
+
+        private string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
+
+        public ICommand CloseLogin { get; set; }
+        public ICommand MinimizeLogin { get; set; }
+        public ICommand Loadwd { get; set; }
+        public ICommand GetIdTab { get; set; }
+        public ICommand MoveWindow { get; set; }
+        public ICommand SwitchTab { get; set; }
+        public string Name;
+        
+        private BitmapImage _Ava;
+        public BitmapImage Ava { get => _Ava; set { _Ava = value; OnPropertyChanged(); } }
         public MainViewModel()
         {
-            SwitchToHomePageCommand = new RelayCommand(SwitchToHomePage);
-            SwitchToOrdersPageCommand = new RelayCommand(SwitchToOrdersPage);
-            SwitchToProductsPageCommand = new RelayCommand(SwitchToProductsPage);
-     
+            CloseLogin = new RelayCommand<MainWindow>((p) => true, (p) => Close());
+            MinimizeLogin = new RelayCommand<MainWindow>((p) => true, (p) => Minimize(p));
+            GetIdTab = new RelayCommand<Button>((p) => true, (p) => Name = p.Uid);
+            MoveWindow = new RelayCommand<MainWindow>((p) => true, (p) => moveWindow(p));
+            SwitchTab = new RelayCommand<MainWindow>((p) => true, (p) => switchtab(p));
         }
 
-        private void SwitchToHomePage(object parameter)
+        public void switchtab(MainWindow p)
         {
-            if (parameter is MainWindow mainWindow)
+            int index = int.Parse(Name);
+            switch (index)
             {
-                mainWindow.Main.Source = new Uri("/view/HomePage.xaml", UriKind.Relative);
+                case 0:
+                    {                    
+                        p.Main.NavigationService.Navigate(new HomePage());
+                        break;
+                    }
+                case 1:
+                    { 
+                        p.Main.NavigationService.Navigate(new Don_Hang());
+                        break;
+                    }
+                case 2:
+                    {                
+                        p.Main.NavigationService.Navigate(new Detail_phone());
+                        break;
+                    }
+                case 3:
+                    {
+                        p.Main.NavigationService.Navigate(new QLSP());
+                        break;
+                    }
+                case 4:
+                    {  
+                        p.Main.NavigationService.Navigate(new QLNH());
+                        break;
+                    }
+                case 5:
+                    {
+                        p.Main.NavigationService.Navigate(new ThongKe());
+                        break;
+                    }
+                case 6:
+                    {
+                        p.Main.NavigationService.Navigate(new QLNV());
+                        break;
+                    }
+                case 7:
+                    {
+                        p.Main.NavigationService.Navigate(new Setting());
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
             }
         }
-
-        private void SwitchToOrdersPage(object parameter)
+        public void moveWindow(MainWindow p)
         {
-            if (parameter is MainWindow mainWindow)
-            {
-                mainWindow.Main.Source = new Uri("/view/Don_Hang.xaml", UriKind.Relative);
-            }
+            p.DragMove();
+        }
+        public void Close()
+        {
+            Application.Current.Shutdown();
         }
 
-        private void SwitchToProductsPage(object parameter)
+        public void Minimize(MainWindow p)
         {
-            if (parameter is MainWindow mainWindow)
-            {
-                mainWindow.Main.Source = new Uri("/view/Detail_phone.xaml", UriKind.Relative);
-            }
+            p.WindowState = WindowState.Minimized;
         }
     }
 }
